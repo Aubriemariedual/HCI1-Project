00e538dd022c11cf3ff78e1e9283e9c8463feb35
Aubriemariedual <aubriemariedual13@gmail.com>
2024-08-02 17:35:25 +0800

    First commit

---
+++ test.py
@@ -0,0 +1,17 @@
+print('Hello, World!')
+
+number_one = 10
+number_two = 13
+
+first_name = 'Kurt'
+
+char_sample = 'c'
+
+people_age ={
+    'Kurt':21,
+    'Mel':20,
+    'Nat':20
+}
+
+total = number_one + number_two
+print('The result is', total)
